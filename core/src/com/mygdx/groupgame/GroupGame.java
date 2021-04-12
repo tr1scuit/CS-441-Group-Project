bb90e179736a451d11acea0028e0170c2782a8ba
Tristan Awayan <jawayan1@binghamton.edu>
2021-04-12 10:03:37 -0400

    test change - tristan

--- core/src/com/mygdx/groupgame/GroupGame.java
+++ core/src/com/mygdx/groupgame/GroupGame.java
@@ -21,7 +21,7 @@ public class GroupGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 10, 20);
 		batch.end();
 	}
 	
